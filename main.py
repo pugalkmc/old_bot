cbcef37bd8a61a6e2e9a56e7abd8339d6d01f990
pugal dev <pugalkmc@gmail.com>
2023-06-11 10:44:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -92,8 +92,10 @@ admins_list = [1155684571, 814546021, 1291659507]
 
 
 async def selva_sheet(update, context, admin=None, date=None):
+    chat_id = update.message.chat_id
+
     collection_name = date if date else datetime.now().strftime("%Y-%m-%d")
-    await bot.send_message(chat_id=1292480260, text=f"request received for date: {collection_name}")
+    await bot.send_message(chat_id=chat_id, text=f"request received for date: {collection_name}")
 
     messages = db.reference(f'selva/{collection_name}').get() or {}
 
@@ -137,7 +139,7 @@ async def selva_sheet(update, context, admin=None, date=None):
 
     wb.save(f"{collection_name}.xlsx")
 
-    await bot.send_document(chat_id=1292480260, document=open(f"{collection_name}.xlsx", "rb"))
+    await bot.send_document(chat_id=chat_id, document=open(f"{collection_name}.xlsx", "rb"))
 
 
 async def save_to_spreadsheet(update=None, context=None, admin=None, date=None):
